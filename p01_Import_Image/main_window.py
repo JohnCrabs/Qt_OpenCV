00e27dce638a3c6c611fbd3012d0b954ccb51d39
JohnCrabs <johncrabs1995@gmail.com>
2020-03-18 20:19:23 +0200

    Create folder named p01_Import_Image (first commit)

---
+++ p01_Import_Image/main_window.py
@@ -0,0 +1,36 @@
+import sys
+from p01_Import_Image.ui_main import *
+
+
+class Window:
+    def __init__(self):
+        # Set flags
+        self.UP = True
+        self.DOWN = False
+
+        # Set up the ui
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.ui = Ui_MainWindow()
+        self.MainWindow = QtWidgets.QMainWindow()
+        self.ui.setupUi(self.MainWindow)
+
+        # Actions list starts here
+        # ------------------------
+        self.ui.actionOpen.triggered.connect(self.open)
+        self.ui.actionSave.triggered.connect(self.save)
+        self.ui.actionExit.triggered.connect(self.exit_window)
+
+        # Actions list ends here
+        # ------------------------
+        self.MainWindow.show()
+        sys.exit(self.app.exec_())
+
+    def open(self):
+        print("open")
+        self.ui.actionSave.setEnabled(self.UP)
+
+    def save(self):
+        print("save")
+
+    def exit_window(self):
+        self.MainWindow.close()  # Close The window
---
+++ p01_Import_Image/ui_main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_main.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(512, 512)
+        MainWindow.setMinimumSize(QtCore.QSize(512, 512))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.img_view = QtWidgets.QLabel(self.centralwidget)
+        self.img_view.setFrameShape(QtWidgets.QFrame.Panel)
+        self.img_view.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.img_view.setText("")
+        self.img_view.setAlignment(QtCore.Qt.AlignCenter)
+        self.img_view.setObjectName("img_view")
+        self.verticalLayout.addWidget(self.img_view)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 512, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setEnabled(False)
+        self.actionSave.setObjectName("actionSave")
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.actionExit)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Test Window "))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
+        self.actionOpen.setText(_translate("MainWindow", "Open"))
+        self.actionOpen.setShortcut(_translate("MainWindow", "Ctrl+O"))
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.actionSave.setShortcut(_translate("MainWindow", "Ctrl+S"))
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
+        self.actionExit.setShortcut(_translate("MainWindow", "Ctrl+Q"))
+
