9b7112aac1e5f6f9af570746905685ece47c6f0d
JohnCrabs <johncrabs1995@gmail.com>
2020-03-20 19:29:07 +0200

    Import and Export Image completed

---
+++ lib/image_filtering.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+class Filters:
+    def __init__(self):
+        self.kernel = []
+
+    def custom(self, cv_mat):
+        pass
+
+    def gaussian(self, cv_mat):
+        pass
+
+    def laplace(self, cv_mat):
+        pass
+
+    def sobel(self, cv_mat):
+        pass
---
+++ lib/image_processing.py
@@ -0,0 +1,7 @@
+import cv2
+
+
+class ImageProcessing:
+    def __init__(self):
+        self.light = 0
+        self.contrast = 1.0
