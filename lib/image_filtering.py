6bf22793987372cee70ac81dafef26bac4cfb9b5
JohnCrabs <johncrabs1995@gmail.com>
2020-03-20 20:09:46 +0200

    Import and Export Image completed

--- lib/image_filtering.py
+++ lib/image_filtering.py
@@ -5,8 +5,11 @@ class Filters:
     def __init__(self):
         self.kernel = []
 
+    def set_kernel(self, kernel):
+        self.kernel = kernel
+    
     def custom(self, cv_mat):
-        pass
+        img = cv2.filter2D(cv_mat, -1, self.kernel)
 
     def gaussian(self, cv_mat):
         pass
