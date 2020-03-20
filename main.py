809ec131089be5554e700392402e42aa7e7ebdd1
JohnCrabs <johncrabs1995@gmail.com>
2020-03-20 02:15:05 +0200

    First Image Processing (create folder)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #from p00_Create_Window.main_window import *
-from p01_Import_Image.main_window import *
+#from p01_Import_Image.main_window import *
+from p02_Simple_Image_Processing.main_window import *
 
 win = Window()
